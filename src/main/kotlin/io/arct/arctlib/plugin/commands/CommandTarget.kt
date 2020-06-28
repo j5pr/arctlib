c98841fc883d16e58780b24823c458cb15360fbb
valkyrienyanko <sebastianbelle074@gmail.com>
2020-06-28 14:49:15 -0500

    Simplify CommandTarget.kt toString()

--- src/main/kotlin/io/arct/arctlib/plugin/commands/CommandTarget.kt
+++ src/main/kotlin/io/arct/arctlib/plugin/commands/CommandTarget.kt
@@ -5,11 +5,5 @@ enum class CommandTarget {
     System,
     Console;
 
-    override fun toString(): String {
-        val targets: MutableMap<CommandTarget, String> = mutableMapOf()
-        for (value in values()) {
-            targets[value] = value.name.toUpperCase()
-        }
-        return targets[this] ?: error("Could not convert CommandTarget to String")
-    }
+    override fun toString(): String = name.toUpperCase()
 }
