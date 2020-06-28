e0594a63dca11cd047a4bc6dcb40afa78783a900
valkyrienyanko <sebastianbelle074@gmail.com>
2020-06-28 16:03:48 -0500

    Cooldown class

---
+++ src/main/kotlin/io/arct/arctlib/utils/Cooldown.kt
@@ -0,0 +1,61 @@
+package io.arct.arctlib.utils
+
+import java.util.*
+
+/**
+ * Simulate a cooldown given a UUID and duration
+ *
+ * @param id The UUID assigned to this cooldown
+ * @param duration The duration of the cooldown in seconds
+ * @constructor Starts the cooldown
+ */
+class Cooldown(id: UUID, val duration: Int) {
+    private val start: Long = System.currentTimeMillis()
+
+    init {
+        cooldowns[id] = this
+    }
+
+    companion object {
+        private val cooldowns: MutableMap<UUID, Cooldown> = mutableMapOf()
+
+        /**
+         * Lets us know if we are in a cooldown or not
+         *
+         * @param id The UUID to check
+         */
+        fun active(id: UUID): Boolean {
+            return if (remaining(id) >= 1) {
+                true
+            } else {
+                stop(id)
+                false
+            }
+        }
+
+        /**
+         * Gets the remaining time left in the cooldown
+         *
+         * @param id The UUID to check
+         */
+        fun remaining(id: UUID): Int {
+            val cooldown: Cooldown? = cooldowns[id]
+            var timeLeft: Int = -1
+            if (cooldown != null) {
+                val now: Long = System.currentTimeMillis()
+                val start: Long = cooldown.start
+                timeLeft = cooldown.duration - ((now - start) / 1000).toInt()
+            }
+            return timeLeft
+        }
+
+        /**
+         * Stops the cooldown
+         *
+         * @param id The UUID of the cooldown to stop
+         */
+        fun stop(id: UUID) {
+            cooldowns.remove(id)
+        }
+    }
+}
