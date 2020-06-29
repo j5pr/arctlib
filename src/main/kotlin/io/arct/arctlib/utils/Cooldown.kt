547a474267f0f09ffd5aece097122923cd2a4a91
valkyrienyanko <sebastianbelle074@gmail.com>
2020-06-28 19:48:16 -0500

    Add command name support to Cooldown.kt

--- src/main/kotlin/io/arct/arctlib/utils/Cooldown.kt
+++ src/main/kotlin/io/arct/arctlib/utils/Cooldown.kt
@@ -6,10 +6,11 @@ import java.util.*
  * Simulate a cooldown given a UUID and duration
  *
  * @param id The UUID assigned to this cooldown
+ * @param command The command that the cooldown is activated on
  * @param duration The duration of the cooldown in seconds
  * @constructor Starts the cooldown
  */
-class Cooldown(id: UUID, val duration: Int) {
+class Cooldown(id: UUID, val command: String, val duration: Int) {
     private val start: Long = System.currentTimeMillis()
 
     init {
@@ -25,8 +26,10 @@ class Cooldown(id: UUID, val duration: Int) {
          * @param id The UUID to check
          * @return If we are active or not
          */
-        fun active(id: UUID): Boolean {
-            return if (remaining(id) > 0) {
+        fun active(id: UUID, command: String): Boolean {
+            val cooldown: Cooldown = cooldowns[id] ?: return false
+
+            return if (remaining(id) > 0 && command == cooldown.command) {
                 true
             } else {
                 stop(id)
@@ -56,8 +59,14 @@ class Cooldown(id: UUID, val duration: Int) {
          *
          * @param id The UUID of the cooldown to stop
          */
-        fun stop(id: UUID) {
-            cooldowns.remove(id)
-        }
+        fun stop(id: UUID) = cooldowns.remove(id)
+
+        /**
+         * Get the cooldown
+         *
+         * @param id The UUID of the cooldown to get
+         * @return Returns the cooldown
+         */
+        fun get(id: UUID): Cooldown? = cooldowns[id]
     }
 }
