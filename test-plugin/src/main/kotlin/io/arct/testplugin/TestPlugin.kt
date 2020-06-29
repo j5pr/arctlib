74d8b430bdc91bdbf72dd4656a3048db7778f308
valkyrienyanko <sebastianbelle074@gmail.com>
2020-06-29 09:51:02 -0500

    Cooldown Commands

--- test-plugin/src/main/kotlin/io/arct/testplugin/TestPlugin.kt
+++ test-plugin/src/main/kotlin/io/arct/testplugin/TestPlugin.kt
@@ -3,12 +3,14 @@ package io.arct.testplugin
 import io.arct.arctlib.plugin.Plugin
 import io.arct.testplugin.commands.CommandTestItems
 import io.arct.testplugin.commands.CommandTestMessage
-import io.arct.testplugin.commands.CommandTestPing
+import io.arct.testplugin.commands.CommandTestCooldown1
+import io.arct.testplugin.commands.CommandTestCooldown2
 
 class TestPlugin : Plugin() {
     override fun enable() {
         command<CommandTestMessage>()
         command<CommandTestItems>()
-        command<CommandTestPing>()
+        command<CommandTestCooldown1>()
+        command<CommandTestCooldown2>()
     }
 }
