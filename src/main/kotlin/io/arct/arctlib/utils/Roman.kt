ccb6c8d6c8c126df6530d289754be5b1248847df
arct <brian@arct.io>
2020-06-28 20:12:43 -0700

    Clean up Roman

--- src/main/kotlin/io/arct/arctlib/utils/Roman.kt
+++ src/main/kotlin/io/arct/arctlib/utils/Roman.kt
@@ -1,22 +1,21 @@
 package io.arct.arctlib.utils
 
-private val map: Map<String, String> = mapOf(
-    "IIIII" to "V",
-    "IIII" to "IV",
-    "VV" to "X",
-    "VIV" to "IX",
-    "XXXXX" to "L",
-    "XXXX" to "XL",
-    "LL" to "C",
-    "LXL" to "XC",
-    "CCCCC" to "D",
-    "CCCC" to "CD",
-    "DD" to "M",
-    "DCD" to "CM"
-)
-
-
 object Roman {
+    private val map: Map<String, String> = mapOf(
+        "IIIII" to "V",
+        "IIII" to "IV",
+        "VV" to "X",
+        "VIV" to "IX",
+        "XXXXX" to "L",
+        "XXXX" to "XL",
+        "LL" to "C",
+        "LXL" to "XC",
+        "CCCCC" to "D",
+        "CCCC" to "CD",
+        "DD" to "M",
+        "DCD" to "CM"
+    )
+
     fun fromInt(n: Int): String {
         var str: String = "I".repeat(n)
 
